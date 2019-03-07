cd24359bad451679108582c65f380657dd65f27c
CodingYuanLiu <lqyuan980413@163.com>
2019-03-07 09:21:50 +0800

    Add book card

--- src/components/Index/Books/book1.vue
+++ src/components/Index/Books/book1.vue
@@ -1,25 +1,25 @@
 <template>
+<router-link to="/details/book1" style="text-decoration: none">
+<el-card shadow="hover" style="border-radius:20px;">
     <div class=book>
     <el-container>
         <el-aside width="160px">
             <div class=bookimage>
-            <router-link to="/details/book1">
             <img src="@/images/icsimage.jpg" style="height:190px"></img>
-            </router-link>
             </div>
         </el-aside>
         <el-main>
             <div class=bookinfo>
-                <router-link to="/details/book1">
                 <div class=title align=left>深入理解计算机基础</div>
-                </router-link>
                 <div class=author align=left>作者：Bryant,Hallaron</div>
-                <div class=price align=left>库存:10 ISBN编号:000</div>
+                <div class=remain align=left>库存:10 ISBN编号:000</div>
                 <div class=price align=left>128￥</div>
             </div>
         </el-main>
     </el-container>
     </div>
+</el-card>
+</router-link>
 </template>
 <style scoped>
     @import './book.css';
--- src/components/Index/Books/book2.vue
+++ src/components/Index/Books/book2.vue
@@ -1,25 +1,25 @@
 <template>
+<router-link to="/details/book2" style="text-decoration: none;">
+<el-card shadow="hover"  style="border-radius:20px;">
     <div class=book>
     <el-container>
         <el-aside width="160px">
             <div class=bookimage>
-            <router-link to="/details/book2">
             <img src="@/images/mingdynasty.jpg" style="height:190px"></img>
-            </router-link>
             </div>
         </el-aside>
         <el-main>
             <div class=bookinfo>
-                <router-link to="/details/book2">
                 <div class=title align=left>明朝那些事儿</div>
-                </router-link>
                 <div class=author align=left>作者：当年明月</div>
-                <div class=price align=left>库存:12 ISBN编号：001</div>
+                <div class=remain align=left>库存:12 ISBN编号：001</div>
                 <div class=price align=left>58￥</div>
             </div>
         </el-main>
     </el-container>
     </div>
+</el-card>
+</router-link>
 </template>
 <style scoped>
     @import './book.css';
